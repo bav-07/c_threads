acee226171eba22ca734cbbd6c17bf79386f3b11
bav-07 <bavaharsan.nagarajah@gmail.com>
2023-09-18 11:31:40 +0100

    added semaphores to producer consumer

--- producer_consumer_mutex.c
+++ producer_consumer_mutex.c
@@ -7,7 +7,7 @@
 #include <sys/sem.h>
 #include <fcntl.h>
 
-#define THREAD_NUM 50
+#define THREAD_NUM 2
 #define BUFFER_CAPACITY 10
 
 int buffer[BUFFER_CAPACITY];
@@ -15,6 +15,9 @@ int itemsInBuffer = 0;
 
 pthread_mutex_t mutexBuffer;
 pthread_cond_t condBuffer;
+
+sem_t *producerSem;
+sem_t *consumerSem;
 
 // print the buffer
 void printBuffer() {
@@ -27,42 +30,57 @@ void printBuffer() {
 
 // add an item to the buffer
 void *produce(void *args) {
-    pthread_mutex_lock(&mutexBuffer);
-    // only add the item if the buffer is not full
-    while (itemsInBuffer == BUFFER_CAPACITY) {
-        printf("(%d P) Waiting to produce...\n", *(int*)args);
-        // wait for the condition variable to be satisfied before exiting this while loop
-        pthread_cond_wait(&condBuffer, &mutexBuffer);
+    while(1) {
+        sem_wait(producerSem);
+        pthread_mutex_lock(&mutexBuffer);
+        // only add the item if the buffer is not full
+        // while (itemsInBuffer == BUFFER_CAPACITY) {
+        //     printf("(%d P) Waiting to produce...\n", *(int*)args);
+        //     // wait for the condition variable to be satisfied before exiting this while loop
+        //     pthread_cond_wait(&condBuffer, &mutexBuffer);
+        // }
+        // set the next item in the buffer to 1 to indicate presence
+        buffer[itemsInBuffer] = 1;
+        // add one to the num of items in the buffer
+        itemsInBuffer++;
+        printf("(%d P) Produced. Items in Buffer: %d\n", *(int*)args, itemsInBuffer);
+        printBuffer();
+        pthread_mutex_unlock(&mutexBuffer);
+        // pthread_cond_broadcast(&condBuffer);
+        // free(args);
+        sem_post(consumerSem);
     }
-    // set the next item in the buffer to 1 to indicate presence
-    buffer[itemsInBuffer] = 1;
-    // add one to the num of items in the buffer
-    itemsInBuffer++;
-    printf("(%d P) Produced. Items in Buffer: %d\n", *(int*)args, itemsInBuffer);
-    printBuffer();
-    pthread_mutex_unlock(&mutexBuffer);
-    pthread_cond_broadcast(&condBuffer);
-    free(args);
+    
 }
 
 void *consume(void *args) {
-    pthread_mutex_lock(&mutexBuffer);
-    // sleep(1);
-    while (itemsInBuffer == 0) {
-        printf("(%d C) Waiting to consume...\n", *(int*)args);
-        pthread_cond_wait(&condBuffer, &mutexBuffer);
+    while (1) {
+        sem_wait(consumerSem);
+        pthread_mutex_lock(&mutexBuffer);
+        // sleep(1);
+        // while (itemsInBuffer == 0) {
+        //     printf("(%d C) Waiting to consume...\n", *(int*)args);
+        //     pthread_cond_wait(&condBuffer, &mutexBuffer);
+        // }
+        buffer[itemsInBuffer - 1] = 0;
+        itemsInBuffer--;
+        printf("(%d C) Consumed. Items in Buffer: %d\n", *(int*)args, itemsInBuffer);
+        printBuffer();
+        pthread_mutex_unlock(&mutexBuffer);
+        // pthread_cond_broadcast(&condBuffer);
+        // free(args);
+        sem_post(producerSem);
     }
-    buffer[itemsInBuffer - 1] = 0;
-    itemsInBuffer--;
-    printf("(%d C) Consumed. Items in Buffer: %d\n", *(int*)args, itemsInBuffer);
-    printBuffer();
-    pthread_mutex_unlock(&mutexBuffer);
-    pthread_cond_broadcast(&condBuffer);
-    free(args);
 }
 
 int main(int argc, char const *argv[])
 {
+    sem_unlink("/produce");
+    sem_unlink("/consume");
+
+    producerSem = sem_open("/produce", O_CREAT, 0660, BUFFER_CAPACITY);
+    consumerSem = sem_open("/consume", O_CREAT, 0660, 0);
+
     pthread_mutex_init(&mutexBuffer, NULL);
     pthread_cond_init(&condBuffer, NULL);
     pthread_t producers[THREAD_NUM];
@@ -91,5 +109,9 @@ int main(int argc, char const *argv[])
     } 
     pthread_mutex_destroy(&mutexBuffer);
     pthread_cond_destroy(&condBuffer);
+
+    sem_unlink("/produce");
+    sem_unlink("/consume");
+
     return 0;
 }
